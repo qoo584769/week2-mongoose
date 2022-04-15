6c6d29d9f3203c44722a7879986b9c87abd5a4e1
ming wu <a85062@pchome.com.tw>
2022-04-16 00:44:24 +0800

    【更新】修復heroku

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { createServer } = require('http');
 const { app } = require('./app.js');
 
 const server = createServer(app);
-const port = 8080;
-server.listen(process.env.PORT || port);
+const port = process.env.PORT || 8080;
+server.listen(port);
